5cdd259d76d104507208ed3e07a51f9ffe33651c
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-07-18 21:08:42 +0530

    Create SQL query for user confirmation rates
    Add SQL query to calculate user confirmation rates

---
+++ LC_1934.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT s.user_id,ROUND((AVG(CASE
+                 WHEN c.action='confirmed' THEN 1
+                 ELSE 0
+                 END)),2) AS confirmation_rate
+FROM Signups as s
+LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id
+ORDER BY s.user_id
+
