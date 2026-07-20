4cc5d0f6355cb2bbc50a060d09ef6cf974ae813f
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-07-20 11:03:17 +0530

    Add SQL query to calculate average product price

---
+++ LC_1251.sql
@@ -0,0 +1,7 @@
+SELECT p.product_id,IFNULL(ROUND(SUM(u.units*p.price)/SUM(u.units),2),0) as average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id=u.product_id
+AND p.start_date<=u.purchase_date
+AND u.purchase_date<=p.end_date
+GROUP BY p.product_id;
